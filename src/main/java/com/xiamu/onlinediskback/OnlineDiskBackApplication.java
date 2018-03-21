3c6934c053c175ff4dba3e1ed9e2c0e3750f3ff4
768840822 <768840822@qq.com>
2018-03-21 13:45:23 +0800

    1111

---
+++ src/main/java/com/xiamu/onlinediskback/OnlineDiskBackApplication.java
@@ -0,0 +1,12 @@
+package com.xiamu.onlinediskback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDiskBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineDiskBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xiamu/onlinediskback/OnlineDiskBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xiamu.onlinediskback;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OnlineDiskBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
